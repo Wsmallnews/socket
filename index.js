c568070cc8b1e02c24e4dfa340924a9412e76142
smallnews <1371606921@qq.com>
2018-05-05 18:24:24 +0800

    socket

--- index.js
+++ index.js
@@ -35,7 +35,14 @@ var io  = require('socket.io')(app);
 //
 // });
 
-io.on('connection', function(socket) {
+// 区别，io.sockets.on 默认namespace 是 /, io.on 是 namespace / 的简写
+// io.sockets.on
+// io.on
+
+var userSocket = io.of('/user');    // user 命名空间
+var socketPool = [];
+
+userSocket.on('connection', function(socket) {
     if (debug) {
         console.log('connection');
     }
@@ -45,31 +52,46 @@ io.on('connection', function(socket) {
     // }, 5000);
     // socket.broadcast.emit({ hello: 'world' });
 
+    // console.log(socket);
 
+    socket.on('user-connect', function (data, cb) {
+        if (data != undefined && data.user != undefined && data.user.id > 0) {
+            var socketId = "socket-" + data.user.id;
+            var is_exist = false;
+            for (let item of socketPool) {
+                if (item.socketId == socketId) {
+                    is_exist = true;
+                }
+            }
 
-    socket.on('my other event', function (data, cb) {
-        console.log(data);
-        cb('smallnews');
-        // console.log(data);
+            if (!is_exist) {        // 将当前用户 放入 socket 连接池
+                socketPool.push({socketId: socketId, socket: socket})
+                console.log(socketPool);
+            }
+        }
+
+        cb({error: 0, info: '连接成功'});
     });
 });
 
 
+setInterval(() => {
+    var socketNum = socketPool.length;
+    console.log(socketNum);
+    if (socketNum > 0) {    // 连接池中有连接
+        var rand = Math.random();
+        console.log(rand);
+
+        if (rand > 0.5){
+            socketPool[0].socket.emit({ hello: 'socket one' });
+        } else {
+            if (socketPool.length > 1) {
+                socketPool[1].socket.emit({ hello: 'socket two' });
+            } else {
+                socketPool[0].socket.emit({ hello: 'socket three' });
+            }
+        }
+    }
+}, 5000)
+
 // io.sockets.emit('String', data);    // 给所有客户端广播消息
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-1

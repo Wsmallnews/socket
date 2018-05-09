20a2b3c5cfb12c87f680df8563bcc964cd200ba4
smallnews <1371606921@qq.com>
2018-05-09 16:32:16 +0800

    redis subscribe

--- index.js
+++ index.js
@@ -54,9 +54,10 @@ userSocket.on('connection', function(socket) {
 
     // console.log(socket);
 
-    socket.on('user-connect', function (data, cb) {
+    socket.on('user-connect', function (data, cb) {     // 监听用户连接
         if (data != undefined && data.user != undefined && data.user.id > 0) {
-            var socketId = "socket-" + data.user.id;
+            var socketId = "SOCKET-" + socket.id;
+            var socketUserId = "SOCKET-" + data.user.id;
             var is_exist = false;
             for (let item of socketPool) {
                 if (item.socketId == socketId) {
@@ -65,33 +66,85 @@ userSocket.on('connection', function(socket) {
             }
 
             if (!is_exist) {        // 将当前用户 放入 socket 连接池
-                socketPool.push({socketId: socketId, socket: socket})
-                console.log(socketPool);
+                socketPool.push({
+                    socketId: socketId,
+                    socketUserId: socketUserId,
+                    socket: socket
+                })
             }
         }
 
         cb({error: 0, info: '连接成功'});
     });
+
+    socket.on('disconnect', function() {
+        for (let i in socketPool) {
+            if (socketPool[i].socketId == "SOCKET-" + socket.id) {
+                socketPool.splice(i, 1);
+                break;
+            }
+        }
+    })
 });
 
 
-setInterval(() => {
-    var socketNum = socketPool.length;
-    console.log(socketNum);
-    if (socketNum > 0) {    // 连接池中有连接
-        var rand = Math.random();
-        console.log(rand);
 
-        if (rand > 0.5){
-            socketPool[0].socket.emit({ hello: 'socket one' });
-        } else {
-            if (socketPool.length > 1) {
-                socketPool[1].socket.emit({ hello: 'socket two' });
-            } else {
-                socketPool[0].socket.emit({ hello: 'socket three' });
-            }
-        }
+
+redis.subscribe('test-channel', function(err, count) {
+    if (debug) {
+        console.log('psubscribe');
     }
-}, 5000)
+});
+
+redis.on('message', function(channel, message) {
+    console.log(channel);
+    console.log(message);
+    // message = JSON.parse(message);
+
+
+    // if (message.event.indexOf('RestartSocketServer') !== -1) {
+    //
+    //     if (debug) {
+    //         console.log('Restart command received');
+    //     }
+    //
+    //     process.exit();
+    //     return;
+    // }
+    //
+    // if (debug) {
+    //     console.log('Message received from event ' + message.event + ' to channel ' + channel);
+    // }
+    console.log(parseInt(message) - 1);
+    console.log(socketPool)
+    console.log("socketId" + socketPool[parseInt(message) - 1].socketId)
+    console.log("socketUserId" + socketPool[parseInt(message) - 1].socketUserId)
+    socketPool[parseInt(message) - 1].socket.emit('user-message', channel + ':这是用户' + message + '的消息');
+});
+
+
+
+
+
+// setInterval(() => {
+//     var socketNum = socketPool.length;
+//
+//     console.log(socketNum);
+//
+//     if (socketNum > 0) {    // 连接池中有连接
+//         var rand = Math.random();
+//         console.log(rand);
+//
+//         if (rand > 0.5){
+//             socketPool[0].socket.emit('user-message', { hello: 'socket one' });
+//         } else {
+//             if (socketPool.length > 1) {
+//                 socketPool[1].socket.emit('user-message', { hello: 'socket two' });
+//             } else {
+//                 socketPool[0].socket.emit('user-message', { hello: 'socket three' });
+//             }
+//         }
+//     }
+// }, 5000)
 
 // io.sockets.emit('String', data);    // 给所有客户端广播消息
